317a0762abf280ce3d4cac873425426c0f5f30d5
JafarbekZokirov <jafarbekzokirovh@gmail.com>
2024-01-13 22:46:11 +0500

    done

---
+++ interpolatsiya.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// const channel = prompt("What's your favourite youtuber?");
+// console.log(`https://t.me/${channel}`);
+
+const name = "Ja'far";
+const job = "Developer";
+
+console.log(`Mening ismim ${name}, mening kasbim esa ${job}`);
+
