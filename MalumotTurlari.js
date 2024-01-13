f979356a5c523bc7a3f2c14d6653d0a7a44c6fe1
JafarbekZokirov <jafarbekzokirovh@gmail.com>
2024-01-13 21:59:32 +0500

    done

---
+++ MalumotTurlari.js
@@ -0,0 +1,44 @@
+"use strict"
+
+//NUMBER
+// let number = 5.6; // Data Type = Number
+// console.log(-8 / 0); // Infinity
+// console.log("Ja'far" * 8); //NaN "Not a number"
+
+//STRING
+const clientName = "Ja'far";
+
+//BOOLEAN 
+const isMarried = false;
+const isCircleEarth = true;
+
+// true = xa | to'g'ri
+// false = yo'q | noto'g'ri
+
+// null
+// console.log(clientAge);
+
+// undifined 
+let age;
+// console.log(age);
+
+// Object 
+
+const thief = {
+    age: 30, //number
+    jacket: "black", /// string
+    isLong: false, // boolean
+};
+
+// console.log(thief.jacket);
+// console.log(thief.age);
+// console.log(thief.isLong);
+// console.log(thief);
+
+
+// Array 
+
+//                0       1     2   3   4
+const colors = ["red", "black", 10, [], {}];
+console.log(colors[1]);
+
