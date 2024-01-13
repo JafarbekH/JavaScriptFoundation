3e9b8bc2c3a80eb732ba8731e91f74efc76328dd
JafarbekZokirov <jafarbekzokirovh@gmail.com>
2024-01-13 22:31:08 +0500

    done

---
+++ foydalanuvchibilanmuloqot.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// alert("Hello Java script")
+
+// const place = confirm("are you here?");
+// place o'zgaruvchisidan 2 xil qiymat keladi true va false
+// console.log(place);
+
+// const joylashuv = prompt("Where are you?", "Ja'far");
+// console.log(joylashuv);
+
+// const age = +prompt("How old are you?", "18");
+// console.log(typeof age);
+
+const favouriteColor = [];
+
+favouriteColor[0] = prompt("What's your favourite color #1", "");
+favouriteColor[1] = prompt("What's your favourite color #2", "");
+favouriteColor[2] = prompt("What's your favourite color #3", "");
+
+console.log(favouriteColor);
