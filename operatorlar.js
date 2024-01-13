c08c79e0fd3a862a8d19a015556f5e918e1709df
JafarbekZokirov <jafarbekzokirovh@gmail.com>
2024-01-13 23:39:12 +0500

    done

---
+++ operatorlar.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// console.log("Jafar" + "Zokirov");
+
+// console.log(6 + +"9");
+
+// inc decr 
+
+let incr = 10;
+let decr = 10;
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(5 % 2);
+
+// console.log(5 * 5 === "25"); // false
+// console.log(5 * 5 == "25"); // true
+
+// && = va || = yoki 
+
+const isAge = true;
+const isClose = true;
+const isChacked = false;
+
+// console.log(isAge || isClose || isChacked);
+// console.log(isAge && isClose && !isChacked);
+
+console.log(2 + 2 * 2 != 8);
+
+
+
