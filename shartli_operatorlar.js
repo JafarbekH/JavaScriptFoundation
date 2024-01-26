cc599b7f6f07b0f9952971a69169c959f338b752
JafarbekZokirov <jafarbekzokirovh@gmail.com>
2024-01-26 09:13:30 +0500

    done

---
+++ shartli_operatorlar.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const age = +prompt("How old are you?", "");
+
+// if (age > 25) {
+//     console.log("Horror film");
+// } else if (age > 18 && age < 25) {
+//     console.log("Boyevik films");
+// } else {
+//     console.log("Cartoon");
+// }
+
+// (age > 18) ? console.log("Hooror film") : console.log("Cartoon");
+
+const namee = "Sherdorr";
+
+switch (namee) {
+    case "Jafar":
+        console.log("Stop");
+        break;
+    case "Sherdor":
+        console.log("Friend");
+        break;
+    default:
+        console.log("Sesiya");
+        break;
+}
