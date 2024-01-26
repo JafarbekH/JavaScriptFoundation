fc9635c55c254553c94b2c524e1665acc5ff19c9
JafarbekZokirov <jafarbekzokirovh@gmail.com>
2024-01-26 15:10:25 +0500

    done

---
+++ funksiya.js
@@ -0,0 +1,34 @@
+"use strict";
+
+console.log("Assalomu alaykum");
+
+// function SalomBer(name) {
+//     console.log(name);
+// }
+
+// SalomBer("Jafar");
+
+// function calc(a, b) {
+//     console.log(a + b);
+// }
+// Function declaration
+// calc(12, 8);
+
+// function calc(a, b) {
+//     return a + b;
+// }
+
+// console.log(calc(12, 8));
+
+// function expression
+// const logger = function () {
+//     console.log("Hello world");
+// }
+
+// logger();
+
+
+// arrow function
+// const calc = (a, b) => a + b;
+// console.log(calc(10, 5));
+
