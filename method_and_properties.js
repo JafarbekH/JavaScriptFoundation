054829e047bc5cc72009fb55ec35db6875e8f7a7
JafarbekZokirov <jafarbekzokirovh@gmail.com>
2024-01-26 17:36:22 +0500

    done

---
+++ method_and_properties.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const user = "Jafar";
+const arr = [1,2,3,4];
+
+// Properties
+// console.log(user.length);
+// console.log(arr.length);
+// console.log(user[2]);
+
+// Method 
+// console.log(user.toUpperCase());
+// console.log(user.toLowerCase());
+
+// const greeting = "Hello World";
+// console.log(greeting.indexOf("Wo"))
+// console.log(greeting.slice(6,9));
+
+
+// const num = 8.8;
+// console.log(Math.round(num));
+
+// const borderWidth = "13.8px";
+// console.log(parseInt(borderWidth));
+// console.log(parseFloat(borderWidth));
