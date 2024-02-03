723ab2ee184d40e873d2daa9c4248b5f84d9b9be
JafarbekZokirov <jafarbekzokirovh@gmail.com>
2024-02-03 09:24:17 +0500

    done

---
+++ dynmaic_typing.js
@@ -0,0 +1,27 @@
+'use strict' 
+
+
+// to string
+// 1-)
+// console.log(typeof String(4))
+
+// //  2-) 
+// console.log(typeof ('string' + 21))
+
+// // to number 
+// // 1-) 
+// console.log(typeof Number('3'))
+
+// // 2-)
+// console.log(typeof +'3');
+
+// // 3-) 
+// console.log(typeof parseInt('5'));
+
+// // to boolean 
+// // 0, '', null, undefined, NaN = xar doim false qaytaradi.
+
+const age = 0
+if (!age) {
+    console.log(typeof age)
+}
