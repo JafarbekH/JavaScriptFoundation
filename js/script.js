de148d8f00875663b8b55d7a9e92d9de64173c57
JafarbekZokirov <jafarbekzokirovh@gmail.com>
2024-02-03 09:59:30 +0500

    script

---
+++ js/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const box = document.getElementById('box')
+// console.log(box);
+
+const buttons = document.getElementsByTagName('button')
+// console.log(buttons);
+
+const circles = document.getElementsByClassName('circle')
+// console.log(circles);
+
+// New Way 
+const wrapper = document.querySelector('.wrapper')
+console.log(wrapper);
+
+const hearts = document.querySelectorAll('.heart')
+// console.log(hearts);
+
+// hearts.forEach((item) => {
+//     console.log(item);
+// })
